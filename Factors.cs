4ff28161bae493a045dec0346f777245542ba70e
dsolerh <dsolerh.cinter95@gmail.com>
2021-10-15 20:50:20 -0400

    Update Factors.cs

--- Factors.cs
+++ Factors.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FactorsLib
 {
     public class Factors
     {
-        public static bool IsPrime(int number) {
+        public static bool IsPrime(int number)
+        {
             for (int i = 2; i < number; i++)
             {
                 if (number % i == 0)
@@ -13,6 +15,30 @@ namespace FactorsLib
                 }
             }
             return true;
+        }
+
+        public static int[] PrimeFactors(int number)
+        {
+            if (IsPrime(number))
+            {
+                return new int[] { number };
+            }
+            List<int> primeFactors = new List<int>();
+
+            while (number != 1)
+            {
+                for (int i = 2; i <= number; i++)
+                {
+                    if (number % i == 0 && IsPrime(i))
+                    {
+                        primeFactors.Add(i);
+                        number /= i;
+                        break;
+                    }
+                }
+            }
+
+            return primeFactors.ToArray();
         }
     }
 }
