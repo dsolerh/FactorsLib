f18e57c94f761298ad3de2059befa06a84b0a251
dsolerh <dsolerh.cinter95@gmail.com>
2021-10-15 20:15:57 -0400

    add IsPrime function

--- Factors.cs
+++ Factors.cs
@@ -4,5 +4,15 @@ namespace FactorsLib
 {
     public class Factors
     {
+        public static bool IsPrime(int number) {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
